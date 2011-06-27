ed9ffa0777863090d5f73c3232fa8c402779f642
Ralf Claussnitzer <ralf.claussnitzer@googlemail.com>
2011-06-27 19:25:48 +0200

    initial commit

---
+++ src/sc_app.erl
@@ -0,0 +1,18 @@
+-module(sc_app).
+
+-behavior(application).
+
+-export([start/2, stop/1]).
+
+start(_StartType, _StartArgs) ->
+	sc_store:init(),
+	case sc_sup:start_link() of
+		{ok, Pid} ->
+			{ok, Pid};
+		Other ->
+			{error, Other}
+	end.
+
+stop(_State) ->
+	ok.
+
